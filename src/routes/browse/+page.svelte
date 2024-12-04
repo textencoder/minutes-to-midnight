ae2d93ebf81e15579a44dacd1b9968524c2c53e4
textencoder <160430049+textencoder@users.noreply.github.com>
2024-12-04 12:26:31 -0600

    minutesxm 1.3.4
    - added hover styles for navbar
    - split nav destinations into page routes
    - created reactive variable for minutes calculation
    - resized center display

---
+++ src/routes/browse/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+
+</script>
+
+<div id="display">
+    <h1 >
+        browse page
+    </h1>
+    <h2>
+        minutes to midnight
+    </h2>
+</div>
+
+<style>
+    #display { 
+        width: 40rem;
+        height: 30rem;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+
+import {onMount} from 'svelte';
+
+let currentDate = new Date();
+let endDate = new Date("January 1, 2025");
+
+$: differenceInMinutes = Math.floor(((endDate - currentDate) / 1000) / 60);
+
+onMount(() => {
+    const intervalId = setInterval(() => {
+        currentDate = new Date();   
+    }, 60000);
+
+    return () => clearInterval(intervalId);
+});
+
+// @ts-ignore
+//let dateEquation = ;
+//let dateArray = dateEquation.toString().split('')
+//let minutesWithComma = dateArray.slice(0,2).join('') + "," + dateArray.slice(2,5).join('');
+
+</script>
+
+<div id="display">
+    <h1 >
+        {differenceInMinutes}
+    </h1>
+    <h2>
+        minutes to midnight
+    </h2>
+</div>
+
+<style>
+    #display { 
+        width: 40rem;
+        height: 30rem;
+        padding: 10px;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        border: 1px solid dodgerblue;
+    }
+
+    h1 {
+        font-size: 9rem;
+    }
+    
+    h2 {
+        font-size: 1.5rem;
+    }
+</style>
