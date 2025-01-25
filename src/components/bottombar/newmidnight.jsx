1279eb0da728ab3a27c32e0d26f8a17102222035
textencoder <160430049+textencoder@users.noreply.github.com>
2025-01-24 23:18:11 -0600

    edits related to mobile design

--- src/components/bottombar/newmidnight.jsx
+++ src/components/bottombar/newmidnight.jsx
@@ -1,6 +1,6 @@
 export default function NewMidnight() {
     return (
-    <button title="New Midnight">
+    <button title="New Midnight" id="new-midnight">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 -960 960 960"
@@ -8,8 +8,8 @@ export default function NewMidnight() {
         >
           <defs>
         <linearGradient id="MyGradient" x1="0%" y1="0%" x2="100%" y2="100%">
-          <stop offset="0%" stopColor="dodgerblue" />
-          <stop offset="100%" stopColor="#7e30cb" />
+          <stop offset="0%" stopColor="white" />
+          <stop offset="100%" stopColor="whitesmoke" />
         </linearGradient>
       </defs>
           <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
--- src/components/bottombar/volume.jsx
+++ src/components/bottombar/volume.jsx
@@ -1,6 +1,6 @@
 export default function Volume() {
     return (
-        <button title="Volume">
+        <button title="Volume" id="volume">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 -960 960 960"
