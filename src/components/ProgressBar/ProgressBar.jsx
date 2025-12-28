f8ba3438e90d756dcedf38f5c21518d3c54644d2
textencoder <160430049+textencoder@users.noreply.github.com>
2025-12-28 09:58:34 -0600

    - add full font weight array to index.html - update progress bar styling - update html structure for scaling - add nav bar with svg icon hover animation

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -2,7 +2,9 @@ const ProgressBar = ({progress}) => {
   return (
     <div className="progress-bar-wrapper">
       <div className="progress-bar">
-        <div className="progress" style={{width: `${progress}%`}}></div>
+        <div className="progress" style={{width: `${progress}%`}}>
+          <p className="google-sans-code">{progress}%</p>
+        </div>
       </div>
     </div>
   );
